e1d37a2f99ba8b194bf556ef75a4a44c0f83e8f6
zaiehilmi <zaiehilmi@gmail.com>
2025-05-05 20:44:59 +0800

    refactor(add_item_screen): extract field handlers into separate methods

--- lib/features/item_management/add_item_screen.dart
+++ lib/features/item_management/add_item_screen.dart
@@ -24,21 +24,103 @@ class AddItemScreen extends HookConsumerWidget {
     final namaController = useTextEditingController();
     final huraianController = useTextEditingController();
     final hargaController = useTextEditingController();
-    final sumberController = useTextEditingController(
-      text: viewModel.senaraiSumber[0],
-    );
     final kategoriController = useTextEditingController();
     final statusController = useTextEditingController();
     final tarikhPembelian = useState<DateTime?>(null);
     final tarikhLuput = useState<DateTime?>(null);
+    final sumberController = useTextEditingController(
+      text: viewModel.senaraiSumber[0],
+    );
 
     final nameFocusNode = useFocusNode(skipTraversal: true);
+
+    final $textStyle = context.textTheme.textStyle.copyWith(fontSize: 14);
 
     useListenable(sumberController);
     useListenable(kategoriController);
     useListenable(statusController);
 
-    final textStyle = context.textTheme.textStyle.copyWith(fontSize: 14);
+    void onTapSumberField() {
+      nameFocusNode.unfocus();
+
+      showPickerModal(
+        context,
+        items: viewModel.senaraiSumber,
+        controller: sumberController,
+        itemLabelBuilder: (String item) => item,
+      );
+    }
+
+    void onTapKategoriField() {
+      nameFocusNode.unfocus();
+
+      showPickerModal(
+        context,
+        items: viewModel.senaraiKategori,
+        controller: kategoriController,
+        itemLabelBuilder: (String item) => item,
+      );
+    }
+
+    void onTapStatusField() {
+      nameFocusNode.unfocus();
+
+      showPickerModal(
+        context,
+        items: viewModel.senaraiStatus,
+        controller: statusController,
+        itemLabelBuilder: (String item) => item,
+      );
+    }
+
+    void onTapTarikhPembelian() {
+      nameFocusNode.unfocus();
+      if (tarikhPembelian.value == null) {
+        tarikhPembelian.value = DateTime.now();
+      }
+
+      showPickerDate<DateTime>(
+        context,
+        date: tarikhPembelian,
+        showClearButton: true,
+      );
+    }
+
+    void onTapTarikhLuput() {
+      nameFocusNode.unfocus();
+
+      showPickerDate<DateTime>(
+        context,
+        date: tarikhLuput,
+        showClearButton: true,
+      );
+    }
+
+    void onChangedInputHarga(String value) {
+      final formattedPrice = formatHarga(value);
+
+      if (formattedPrice != value) {
+        hargaController.value = TextEditingValue(
+          text: formattedPrice,
+          selection: TextSelection.collapsed(offset: formattedPrice.length),
+        );
+      }
+    }
+
+    void onPressedSimpan() {
+      if (formKey.value.currentState?.validate() ?? false) {
+        viewModelNotifier
+          ..setNama(namaController.text)
+          ..setHuraian(huraianController.text)
+          ..setHarga(hargaController.text)
+          ..setSumber(sumberController.text)
+          ..setKategori(kategoriController.text)
+          ..setStatus(statusController.text)
+          ..setTarikhPembelian(tarikhPembelian.value)
+          ..setTarikhLuput(tarikhLuput.value)
+          ..onSubmit();
+      }
+    }
 
     useEffect(() {
       nameFocusNode.requestFocus();
@@ -50,11 +132,7 @@ class AddItemScreen extends HookConsumerWidget {
       title: const Text('Tambah Item Baharu'),
       trailing: CupertinoButton(
         padding: EdgeInsets.zero,
-        onPressed: () {
-          if (formKey.value.currentState?.validate() ?? false) {
-            viewModelNotifier.onSubmit();
-          }
-        },
+        onPressed: onPressedSimpan,
         child: const Text('Simpan'),
       ),
       child: Form(
@@ -71,7 +149,7 @@ class AddItemScreen extends HookConsumerWidget {
                   focusNode: nameFocusNode,
                   prefix: PrefixInTextForm(text: 'Nama', isRequired: true),
                   placeholder: 'Shokubutsu sabun mandi',
-                  style: textStyle,
+                  style: $textStyle,
                   textCapitalization: TextCapitalization.sentences,
                   autocorrect: false,
                   autovalidateMode: AutovalidateMode.onUnfocus,
@@ -84,11 +162,9 @@ class AddItemScreen extends HookConsumerWidget {
                 ),
                 CupertinoTextFormFieldRow(
                   controller: huraianController,
-                  initialValue: viewModel.huraian,
-                  onChanged: viewModelNotifier.setHuraian,
                   prefix: PrefixInTextForm(text: 'Huraian'),
                   placeholder: 'Huraian',
-                  style: textStyle,
+                  style: $textStyle,
                   textCapitalization: TextCapitalization.sentences,
                   autocorrect: false,
                 ),
@@ -101,19 +177,11 @@ class AddItemScreen extends HookConsumerWidget {
                   title: PrefixInTextForm(text: 'Sumber'),
                   additionalInfo: placeholderForPicker(
                     value: sumberController.text,
-                    style: textStyle,
+                    style: $textStyle,
                     placeholder: 'Sila pilih',
                   ),
                   trailing: CupertinoListTileChevron(),
-                  onTap: () {
-                    nameFocusNode.unfocus();
-                    showPickerModal(
-                      context,
-                      items: viewModel.senaraiSumber,
-                      controller: sumberController,
-                      itemLabelBuilder: (String item) => item,
-                    );
-                  },
+                  onTap: onTapSumberField,
                 ),
               ],
             ),
@@ -126,57 +194,29 @@ class AddItemScreen extends HookConsumerWidget {
                   controller: hargaController,
                   prefix: PrefixInTextForm(text: 'RM'),
                   placeholder: '0.00',
-                  style: textStyle,
+                  style: $textStyle,
                   keyboardType: TextInputType.number,
-                  onChanged: (value) {
-                    final formattedPrice = formatHarga(value);
-
-                    if (formattedPrice != value) {
-                      hargaController.value = TextEditingValue(
-                        text: formattedPrice,
-                        selection: TextSelection.collapsed(
-                          offset: formattedPrice.length,
-                        ),
-                      );
-                    }
-                  },
+                  onChanged: onChangedInputHarga,
                 ),
                 CupertinoListTile(
                   title: PrefixInTextForm(text: 'Kategori'),
                   additionalInfo: placeholderForPicker(
                     value: kategoriController.text,
-                    style: textStyle,
+                    style: $textStyle,
                     placeholder: 'Sila pilih',
                   ),
                   trailing: CupertinoListTileChevron(),
-                  onTap: () {
-                    nameFocusNode.unfocus();
-                    showPickerModal(
-                      context,
-                      items: viewModel.senaraiKategori,
-                      controller: kategoriController,
-                      itemLabelBuilder: (String item) => item,
-                    );
-                  },
+                  onTap: onTapKategoriField,
                 ),
                 CupertinoListTile(
                   title: PrefixInTextForm(text: 'Status'),
                   additionalInfo: placeholderForPicker(
                     value: statusController.text,
-                    style: textStyle,
+                    style: $textStyle,
                     placeholder: 'Sila pilih',
                   ),
                   trailing: CupertinoListTileChevron(),
-                  onTap: () {
-                    nameFocusNode.unfocus();
-
-                    showPickerModal(
-                      context,
-                      items: viewModel.senaraiStatus,
-                      controller: statusController,
-                      itemLabelBuilder: (String item) => item,
-                    );
-                  },
+                  onTap: onTapStatusField,
                 ),
               ],
             ),
@@ -186,39 +226,21 @@ class AddItemScreen extends HookConsumerWidget {
                   title: PrefixInTextForm(text: 'Tarikh beli', width: 70),
                   additionalInfo: placeholderForPicker(
                     value: tarikhPembelian.value.tarikhNumeral,
-                    style: textStyle,
+                    style: $textStyle,
                     placeholder: 'Sila pilih tarikh',
                   ),
                   trailing: CupertinoListTileChevron(),
-                  onTap: () {
-                    nameFocusNode.unfocus();
-                    if (tarikhPembelian.value == null) {
-                      tarikhPembelian.value = DateTime.now();
-                    }
-
-                    showPickerDate<DateTime>(
-                      context,
-                      date: tarikhPembelian,
-                      showClearButton: true,
-                    );
-                  },
+                  onTap: onTapTarikhPembelian,
                 ),
                 CupertinoListTile(
                   title: PrefixInTextForm(text: 'Tarikh luput', width: 70),
                   additionalInfo: placeholderForPicker(
                     value: tarikhLuput.value.tarikhNumeral,
-                    style: textStyle,
+                    style: $textStyle,
                     placeholder: 'Sila pilih tarikh',
                   ),
                   trailing: CupertinoListTileChevron(),
-                  onTap: () {
-                    nameFocusNode.unfocus();
-                    showPickerDate<DateTime>(
-                      context,
-                      date: tarikhLuput,
-                      showClearButton: true,
-                    );
-                  },
+                  onTap: onTapTarikhLuput,
                 ),
               ],
             ),
